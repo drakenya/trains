9060f884e2137cd22f336f30ba1583e6bf087370
Andrew Kroll <krolla@gmail.com>
2018-05-17 20:08:50 -0400

    Allow adding of full waybills (oops)

--- src/Form/FullWaybillType.php
+++ src/Form/FullWaybillType.php
@@ -16,30 +16,63 @@ class FullWaybillType extends AbstractType
     {
         $builder
             ->add('carCard', null, [
+                'choice_label' => function (CarCard $carCard) {
+                    return sprintf('%s %s', $carCard->getRailroad()->getReportingMark(), $carCard->getCarNumber());
+                },
                 'query_builder' => function (EntityRepository $repository) {
                     return $repository
-                        ->createQueryBuilder('cc')
-                        ->addOrderBy('cc.reportingMark', 'ASC')
-                        ->addOrderBy('cc.carNumber', 'ASC')
+                        ->createQueryBuilder('carCard')
+                        ->innerJoin('carCard.railroad', 'railroad')
+                        ->addOrderBy('railroad.reportingMark', 'ASC')
+                        ->addOrderBy('carCard.carNumber', 'ASC')
                     ;
                 },
-                'group_by' => function (CarCard $carCard, $key, $index) {
-                    return $carCard->getAarType();
+                'group_by' => function (CarCard $carCard) {
+                    return $carCard->getAarCode()->getCode();
                 },
             ])
             ->add('waybill', null, [
+                'choice_label' => function (Waybill $waybill) {
+                    return sprintf(
+                        '%s -> %s (%s)',
+                        $waybill->getShipper()
+                            ? sprintf(
+                                '%s / %s, %s',
+                                $waybill->getShipper()->getName(),
+                                $waybill->getShipper()->getLocation()->getStationName(),
+                                $waybill->getShipper()->getLocation()->getState()
+                            )
+                            : null,
+                        $waybill->getConsignee()
+                            ? sprintf(
+                            '%s / %s, %s',
+                            $waybill->getConsignee()->getName(),
+                            $waybill->getConsignee()->getLocation()->getStationName(),
+                            $waybill->getConsignee()->getLocation()->getState()
+                        )
+                            : null,
+                        $waybill->getLadingDescription()
+                    );
+                },
                 'query_builder' => function (EntityRepository $repository) {
                     return $repository
-                        ->createQueryBuilder('w')
-                        ->addOrderBy('w.aarClass', 'ASC')
-                        ->addOrderBy('w.fromAddress', 'ASC')
-                        ->addOrderBy('w.shipper', 'ASC')
-                        ->addOrderBy('w.toAddress', 'ASC')
-                        ->addOrderBy('w.consignee', 'ASC')
+                        ->createQueryBuilder('waybill')
+                        ->leftJoin('waybill.aarCode', 'aarCode')
+                        ->leftJoin('waybill.consignee', 'consignee')
+                        ->leftJoin('consignee.location', 'consignee_location')
+                        ->leftJoin('waybill.shipper', 'shipper')
+                        ->leftJoin('shipper.location', 'shipper_location')
+                        ->addOrderBy('aarCode.code', 'ASC')
+                        ->addOrderBy('shipper_location.state', 'ASC')
+                        ->addOrderBy('shipper_location.stationName', 'ASC')
+                        ->addOrderBy('shipper.name', 'ASC')
+                        ->addOrderBy('consignee_location.state', 'ASC')
+                        ->addOrderBy('consignee_location.stationName', 'ASC')
+                        ->addOrderBy('consignee.name', 'ASC')
                         ;
                 },
-                'group_by' => function (Waybill $waybill, $key, $index) {
-                    return $waybill->getAarClass();
+                'group_by' => function (Waybill $waybill) {
+                    return $waybill->getAarCode()->getCode();
                 },
             ])
         ;
